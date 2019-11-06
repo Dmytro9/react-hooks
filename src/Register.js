b52e7049482e8a7fbb31d304583cf8e76b1f3bb3
Dmytro Poliuliuk <dmytro.poliuliuk@ironsrc.com>
2019-11-06 16:57:10 +0200

    Register

--- src/Register.js
+++ src/Register.js
@@ -1,13 +1,29 @@
 import React, { useState } from 'react';
 
+const initialFormState = {
+    username: '',
+    email: '',
+    password: ''
+}
 
 export default function Register() {
 
-    const [form, setForm] = useState({
-        username: '',
-        email: '',
-        password: ''
-    })
+    const [form, setForm] = useState(initialFormState)
+    const [user, setUser] = useState(null)
+
+    const handleChange = event => {
+        setForm({
+            ...form,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault()
+       
+        setUser(form)
+        setForm(initialFormState)
+    }
 
     return (
         <div
@@ -22,21 +38,32 @@ export default function Register() {
                     alignItems: "center",
                     justifyItems: "center"
                 }}
+                onSubmit={handleSubmit}
             >
                 <input 
                     type="text" 
                     placeholder="Username"
+                    name='username'
+                    onChange={handleChange}
+                    value={form.username}
                 />
                 <input 
                     type="email" 
                     placeholder="Email addres"
+                    name='email'
+                    onChange={handleChange}
+                    value={form.email}
                 />
                 <input 
                     type="text" 
                     placeholder="Password"
+                    name='password'
+                    onChange={handleChange}
+                    value={form.password}
                 />
                 <button type="submit">Submit </button>
             </form>    
+            {user && JSON.stringify(user, null, 2)}
         </div>
     )
 }
