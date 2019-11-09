f737a38e0ea47dcb0a32ec07e8e8b18e051a2b87
Dmytro9 <dpoliuliuk@gmail.com>
2019-11-09 20:12:41 +0200

    fetching data error handling loader

--- src/FetchApi.js
+++ src/FetchApi.js
@@ -1,25 +1,67 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 
 export default function FetchApi() {
 
     const [results, setResults] = useState([]);
+    const [query, setQuery] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const searchInputRef = useRef();
 
     useEffect(() => {
         fetchData();
-      }, []);
+    }, []);
 
     const fetchData = async () => {
-        const responce = await axios.get('https://parallelum.com.br/fipe/api/v1/carros/marcas')
-        setResults(responce.data);
+        setLoading(true);
+        try {
+            const responce = await axios
+                .get(`https://parallelum.com.br/fipe/api/v1/carros/marcas?q=${query}`)
+            setResults(responce.data);
+        } catch (err) {
+            setError(err);
+        }
+        setLoading(false);
+    }
+
+    const handleChange = (event) => {
+        setQuery(event.target.value);
+    }
+
+    const handleSearch = (event) => {
+        event.preventDefault();
+        fetchData();
+    }
+
+    const handleClearSearch = () => {
+        setQuery('');
+        searchInputRef.current.focus();
     }
 
     return (
-        <ul>
-        {results.map(result => (
-            <li key={result.codigo}>{result.nome} - {result.codigo}</li>
-        ))}
-        </ul>
+        <>
+            <form onSubmit={handleSearch}> {/* to make search by pressing Enter bnt */} 
+                <input 
+                    type='text' 
+                    onChange={handleChange} 
+                    value={query} 
+                    ref={searchInputRef}
+                />
+                <button type='submit' onClick={fetchData}>Search</button> 
+                <button type='button' onClick={handleClearSearch}>Clear</button> 
+            </form>
+            {
+                loading ? (<div>Loading results ...</div>) :
+                    <ul>
+                    {results.map(result => (
+                        <li key={result.codigo}>{result.nome} - {result.codigo}</li>
+                    ))}
+                    </ul>
+            }
+               
+            { error ? <div>{error.message}</div> : null }
+        </>
     )
 }
