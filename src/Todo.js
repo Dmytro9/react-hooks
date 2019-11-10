2364f624e7124000d88405065f8abbb81c079b80
Dmytro9 <dpoliuliuk@gmail.com>
2019-11-10 10:11:07 +0200

    useReducer

--- src/Todo.js
+++ src/Todo.js
@@ -1,15 +1,56 @@
-import React, { useContext } from 'react';
+import React, { useContext, useReducer } from 'react';
 import { UserContext } from './index';
 
 
+const initialState = {
+  count: 0
+}
+
+function reducer(state, action) {
+  switch(action.type) {
+
+    case 'increment':
+      return {
+        count: state.count + 1
+      }
+
+    case 'decrement':
+      return {
+        count: state.count - 1
+      }
+
+    case 'reset':
+      return initialState
+
+    default:
+      return initialState;
+  }
+}
+
 export default function Todo({username}) {
 
+  const [state, dispatch] = useReducer(reducer, initialState);
   const value = useContext(UserContext);
 
   return (
     <>
       <div>
-        Hello, {value}
+        Count: {state.count}
+        <button
+          onClick={() => dispatch({type: 'increment'})}
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => dispatch({type: 'decrement'})}
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => dispatch({type: 'reset'})}
+        >
+          Reset
+        </button>
       </div>
     </>
   )
